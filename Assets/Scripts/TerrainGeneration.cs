9adb03af3ef2184a985293563688743f626cb9f7
Junhao Zhang <junhaodev@gmail.com>
2022-01-18 22:03:28 -0600

    Perlin noise generation!

--- Assets/Scripts/TerrainGeneration.cs
+++ Assets/Scripts/TerrainGeneration.cs
@@ -11,7 +11,7 @@ public class TerrainGeneration : MonoBehaviour
             for (int z = 0; z < 10; z++)
             {
                 GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cube.transform.position = new Vector3(x, 0f, z);
+                cube.transform.position = new Vector3(x, Mathf.Floor(Mathf.PerlinNoise((float)x / 10, (float)z / 10) * 10), z);
                 cube.GetComponent<MeshRenderer>().material = materials[Random.Range(0, materials.Length)];
             }
         }
